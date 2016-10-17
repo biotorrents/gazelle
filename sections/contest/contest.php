2ee71683839d1f60ec5dae5343ebf9f441f53f45
spaghetti <spaghetti@oppaiti.me>
2016-10-17 19:19:09 -0400

    Don't break contest page if there's no contest settings

--- sections/contest/contest.php
+++ sections/contest/contest.php
@@ -15,10 +15,10 @@ if (!($ContestSettings = $Cache->get_value("contest_settings"))) {
     list($Amount, $Currency)   = $DB->next_record();
   }
   $ContestSettings = array(
-    'query'  => html_entity_decode($QueryPart),
+    'query'  => $QueryPart ?? '1=2',
     'rules'  => $Rules,
-    'start'  => $StartTime,
-    'end'    => $EndTime,
+    'start'  => $StartTime ?? 0,
+    'end'    => $EndTime ?? 0,
     'reward' => ($Amount.' '.$Currency.'/torrent')
   );
   $Cache->cache_value('contest_settings', $ContestSettings);
@@ -47,53 +47,57 @@ if (!($Scores = $Cache->get_value("contest_scores"))) {
 
 View::show_header('Contest');
 
-if (time() < $ContestSettings['start']) {
-  print '<h2>Future Contest (Starts in '.time_diff($ContestSettings['start'],2,false).')</h2>';
-} else if (time() > $ContestSettings['end']) {
-  print '<h2>Finished Contest</h2>';
+if (!$ContestSettings['start'] || !$ContestSettings['end']) {
+  print '<h2>No Contests</h2>';
 } else {
-  print '<h2>Ongoing Contest! ('.time_diff($ContestSettings['end'],2,false).' remaining)</h2>';
-}
-?>
+  if (time() < $ContestSettings['start']) {
+    print '<h2>Future Contest (Starts in '.time_diff($ContestSettings['start'],2,false).')</h2>';
+  } else if (time() > $ContestSettings['end']) {
+    print '<h2>Finished Contest</h2>';
+  } else {
+    print '<h2>Ongoing Contest! ('.time_diff($ContestSettings['end'],2,false).' remaining)</h2>';
+  }
+  ?>
 
-<div class="thin flex">
-  <div class="box pad grow">
-<? if ($Scores) { ?>
-    <h2 id="general">Scoreboard</h2>
-    <table width="100%" class="contest_scoreboard">
-      <tr class="colhead">
-        <td>Place</td>
-        <td>User</td>
-        <td>Score</td>
-      </tr>
-<?   foreach ($Scores as $Place => $Score) { ?>
-  <tr class="row">
-    <td><?=($Place+1)?></td>
-    <td><a href="/user.php?id=<?=$Score['ID']?>"><?=$Score['Username']?></a></td>
-    <td><?=$Score['Uploads']?></td>
-  </tr>
-<?   } ?>
-    </table>
-<? } else { ?>
-    <h2>No Scores Yet</h2>
-<? } ?>
-  </div>
-  <div class="shrink flex" style="margin-left: 1em; flex-direction: column;">
-    <div class="box pad">
-      <h2>Qualifications</h2>
-      <ul>
-<?
-  print '<li>'.str_replace('\n', '</li><li>', $ContestSettings['rules']).'</li>'
-?>
-      </ul>
+  <div class="thin flex">
+    <div class="box pad grow">
+  <? if ($Scores) { ?>
+      <h2 id="general">Scoreboard</h2>
+      <table width="100%" class="contest_scoreboard">
+        <tr class="colhead">
+          <td>Place</td>
+          <td>User</td>
+          <td>Score</td>
+        </tr>
+  <?   foreach ($Scores as $Place => $Score) { ?>
+    <tr class="row">
+      <td><?=($Place+1)?></td>
+      <td><a href="/user.php?id=<?=$Score['ID']?>"><?=$Score['Username']?></a></td>
+      <td><?=$Score['Uploads']?></td>
+    </tr>
+  <?   } ?>
+      </table>
+  <? } else { ?>
+      <h2>No Scores Yet</h2>
+  <? } ?>
     </div>
-    <div class="box pad">
-      <h2>Rewards</h2>
-      <ul>
-        <li><?=$ContestSettings['reward']?></li>
-      </ul>
+    <div class="shrink flex" style="margin-left: 1em; flex-direction: column;">
+      <div class="box pad">
+        <h2>Qualifications</h2>
+        <ul>
+  <?
+    print '<li>'.str_replace('\n', '</li><li>', $ContestSettings['rules']).'</li>'
+  ?>
+        </ul>
+      </div>
+      <div class="box pad">
+        <h2>Rewards</h2>
+        <ul>
+          <li><?=$ContestSettings['reward']?></li>
+        </ul>
+      </div>
     </div>
   </div>
-</div>
 
-<? View::show_footer(); ?>
+<? }
+ View::show_footer(); ?>
