81516af856c66197b177561484bd572885f20e07
spaghetti <spaghetti@oppaiti.me>
2016-12-18 15:54:06 -0500

    Typo in last scheduler change

--- sections/schedule/hourly/sphinx_min_max_matches.php
+++ sections/schedule/hourly/sphinx_min_max_matches.php
@@ -4,7 +4,7 @@ $SphQL->where_match('_all', 'fake', false);
 $SphQL->select('id')->from('torrents, delta')->limit(0,0,10000);
 $TTorrents = $SphQL->query()->get_meta('total_found');
 
-$SphQL->select('groupid')->group_by('groupid')$SphQL->from('torrents, delta')->limit(0,0,10000);
+$SphQL->select('groupid')->group_by('groupid')->from('torrents, delta')->limit(0,0,10000);
 $TGroups = $SphQL->query()->get_meta('total_found');
 
 $Cache->cache_value('sphinx_min_max_matches', 2*($TTorrents-$TGroups));
