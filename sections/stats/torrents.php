18934dddc6cadfd93bc69321a7b813f8b6086b22
spaghetti <spaghetti@oppaiti.me>
2017-02-11 22:37:33 -0500

    Remove google chart calls from torrents stat page

--- sections/stats/torrents.php
+++ sections/stats/torrents.php
@@ -1,7 +1,7 @@
 <?
-if (!list($Labels, $InFlow, $OutFlow, $NetFlow, $Max) = $Cache->get_value('torrents_timeline')) {
+if (!list($Labels, $InFlow, $OutFlow, $Max) = $Cache->get_value('torrents_timeline')) {
   $DB->query("
-    SELECT DATE_FORMAT(Time,'%b \'%y') AS Month, COUNT(ID)
+    SELECT DATE_FORMAT(Time,\"%b %Y\") AS Month, COUNT(ID)
     FROM log
     WHERE Message LIKE 'Torrent % was uploaded by %'
     GROUP BY Month
@@ -9,84 +9,79 @@ if (!list($Labels, $InFlow, $OutFlow, $NetFlow, $Max) = $Cache->get_value('torre
     LIMIT 1, 12");
   $TimelineIn = array_reverse($DB->to_array());
   $DB->query("
-    SELECT DATE_FORMAT(Time,'%b \'%y') AS Month, COUNT(ID)
+    SELECT DATE_FORMAT(Time,\"%b %Y\") AS Month, COUNT(ID)
     FROM log
     WHERE Message LIKE 'Torrent % was deleted %'
     GROUP BY Month
     ORDER BY Time DESC
     LIMIT 1, 12");
   $TimelineOut = array_reverse($DB->to_array());
+
+  foreach ($TimelineIn as $Month) {
+    list($Labels[], $InFlow[]) = $Month;
+  }
+  foreach ($TimelineOut as $Month) {
+    list(, $OutFlow[]) = $Month;
+  }
+  $Cache->cache_value('torrents_timeline', array($Labels, $InFlow, $OutFlow, $Max), mktime(0, 0, 0, date('n') + 1, 2)); //Tested: fine for dec -> jan
+}
+
+if (!$CategoryDistribution = $Cache->get_value('category_distribution')) {
   $DB->query("
-    SELECT DATE_FORMAT(Time,'%b \'%y') AS Month, COUNT(ID)
-    FROM torrents
-    GROUP BY Month
-    ORDER BY Time DESC
-    LIMIT 1, 12");
-  $TimelineNet = array_reverse($DB->to_array());
-
-  foreach ($TimelineIn as $Month) {
-    list($Label, $Amount) = $Month;
-    if ($Amount > $Max) {
-      $Max = $Amount;
-    }
-  }
-  foreach ($TimelineOut as $Month) {
-    list($Label, $Amount) = $Month;
-    if ($Amount > $Max) {
-      $Max = $Amount;
-    }
-  }
-  foreach ($TimelineNet as $Month) {
-    list($Label, $Amount) = $Month;
-    if ($Amount > $Max) {
-      $Max = $Amount;
-    }
-  }
-  foreach ($TimelineIn as $Month) {
-    list($Label, $Amount) = $Month;
-    $Labels[] = $Label;
-    $InFlow[] = number_format(($Amount / $Max) * 100, 4);
-  }
-  foreach ($TimelineOut as $Month) {
-    list($Label, $Amount) = $Month;
-    $OutFlow[] = number_format(($Amount / $Max) * 100, 4);
-  }
-  foreach ($TimelineNet as $Month) {
-    list($Label, $Amount) = $Month;
-    $NetFlow[] = number_format(($Amount / $Max) * 100, 4);
-  }
-  $Cache->cache_value('torrents_timeline', array($Labels, $InFlow, $OutFlow, $NetFlow, $Max), mktime(0, 0, 0, date('n') + 1, 2)); //Tested: fine for dec -> jan
+    SELECT tg.CategoryID, COUNT(t.ID) AS Torrents
+    FROM torrents AS t
+      JOIN torrents_group AS tg ON tg.ID = t.GroupID
+    GROUP BY tg.CategoryID
+    ORDER BY Torrents DESC");
+  $CategoryDistribution = $DB->to_array();
+  $Cache->cache_value('category_distribution', $CategoryDistribution, 3600 * 24 * 14);
+}
+foreach ($CategoryDistribution as $i => $Category) {
+  list($CategoryID, $Torrents) = $Category;
+  $CategoryDistribution[$i]['CategoryID'] = $Categories[$CategoryID - 1];
 }
 
-include_once(SERVER_ROOT.'/classes/charts.class.php');
-$DB->query("
-  SELECT tg.CategoryID, COUNT(t.ID) AS Torrents
-  FROM torrents AS t
-    JOIN torrents_group AS tg ON tg.ID = t.GroupID
-  GROUP BY tg.CategoryID
-  ORDER BY Torrents DESC");
-$Groups = $DB->to_array();
-$Pie = new PIE_CHART(750, 400, array('Other' => 1, 'Percentage' => 1));
-foreach ($Groups as $Group) {
-  list($CategoryID, $Torrents) = $Group;
-  $CategoryName = $Categories[$CategoryID - 1];
-  $Pie->add($CategoryName, $Torrents);
-}
-$Pie->transparent();
-$Pie->color('FF33CC');
-$Pie->generate();
-$Categories = $Pie->url();
-
-View::show_header('Detailed torrent statistics');
+View::show_header('Detailed torrent statistics', 'chart');
 ?>
 
+<h3 id="Upload_Flow"><a href="#Upload_Flow">Uploads by month</a></h3>
 <div class="box pad center">
-  <h1>Uploads by month</h1>
-  <img src="https://chart.googleapis.com/chart?cht=lc&amp;chs=880x160&amp;chco=000D99,99000D,00990D&amp;chg=0,-1,1,1&amp;chxt=y,x&amp;chxs=0,h&amp;chxl=1:|<?=implode('|', $Labels)?>&amp;chxr=0,0,<?=$Max?>&amp;chd=t:<?=implode(',', $InFlow)?>|<?=implode(',', $OutFlow)?>|<?=implode(',', $NetFlow)?>&amp;chls=2,4,0&amp;chdl=Uploads|Deletions|Remaining&amp;chf=bg,s,FFFFFF00" alt="User Flow Chart" />
+  <canvas class="chart" id="chart_torrents_timeline"></canvas>
+  <script>
+    new Chart($('#chart_torrents_timeline').raw().getContext('2d'), {
+      type: 'line',
+      data: {
+        labels: <? print '["'.implode('","',$Labels).'"]'; ?>,
+        datasets: [ {
+          label: "New Torrents",
+          backgroundColor: "rgba(0,0,255,0.2)",
+          borderColor: "rgba(0,0,255,0.8)",
+          data: <? print "[".implode(",",$InFlow)."]"; ?>
+        }, {
+          label: "Deleted Torrents",
+          backgroundColor: "rgba(255,0,0,0.2)",
+          borderColor: "rgba(255,0,0,0.8)",
+          data: <? print "[".implode(",",$OutFlow)."]"; ?>
+        }]
+      }
+    })
+  </script>
 </div>
+<h3 id="Torrent_Categories"><a href="#Torrent_Categories">Torrents by category</a></h3>
 <div class="box pad center">
-  <h1>Torrents by category</h1>
-  <img src="<?=$Categories?>" alt="" />
+  <canvas class="chart" id="chart_torrent_categories"></canvas>
+  <script>
+    new Chart($('#chart_torrent_categories').raw().getContext('2d'), {
+      type: 'pie',
+      data: {
+        labels: <? print '["'.implode('","', array_column($CategoryDistribution, 'CategoryID')).'"]'; ?>,
+        datasets: [ {
+          data: <? print "[".implode(",", array_column($CategoryDistribution, 'Torrents'))."]"; ?>,
+          backgroundColor: ['#8a00b8','#a944cb','#be71d8','#e8ccf1', '#f3e3f9', '#fbf6fd', '#ffffff']
+        }]
+      }
+    })
+  </script>
 </div>
 <?
 View::show_footer();
