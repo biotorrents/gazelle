daa111dfb04837370217591d71f49b0a37e0cd6f
spaghetti <spaghetti@oppaiti.me>
2017-10-04 19:01:06 -0400

    [Schedule] Remove useless code and clean up
    Thanks Apollo dev

--- sections/schedule/index.php
+++ sections/schedule/index.php
@@ -16,7 +16,7 @@ if ($PCount > 3) {  // 3 because the cron job starts two processes and pgrep fin
 $AS = check_perms('admin_schedule');
 
 function run_all_in($Dir) {
-  $Tasks = array_diff(scandir(SERVER_ROOT.'/sections/schedule/'.$Dir, 1), array('.', '..'));
+  $Tasks = array_diff(scandir(SERVER_ROOT.'/sections/schedule/'.$Dir, 1), ['.', '..']);
   extract($GLOBALS);
   foreach ($Tasks as $Task) {
     $TimeStart = microtime(true);
@@ -37,63 +37,63 @@ if ($AS) {
   echo '<div class="box">';
 }
 
+$ASBreak = $AS ? "<br>" : "\n";
+
 $DB->query("
   SELECT NextHour, NextDay, NextBiWeekly
   FROM schedule");
 list($Hour, $Day, $BiWeek) = $DB->next_record();
 
-$NextHour = date('H', time(date('H') + 1, 0, 0, date('m'), date('d'), date('Y')));
-$NextDay = date('d', time(0, 0, 0, date('m'), date('d') + 1, date('Y')));
+$NextHour = date('H');
+$NextDay = date('d');
 $NextBiWeek = (date('d') < 22 && date('d') >= 8) ? 22 : 8;
 
 $DB->query("
   UPDATE schedule
   SET NextHour = $NextHour, NextDay = $NextDay, NextBiWeekly = $NextBiWeek");
 
-$sqltime = sqltime();
-
-echo "$sqltime".($AS?"<br>":"\n");
+echo date('Y-m-d H:i:s').$ASBreak;
 
 //-------------- Run every time ------------------------------------------//
 if (!(isset($_GET['notevery']) && $_GET['notevery'])) {
   run_all_in('every');
-  echo "Ran every-time functions".($AS?'<br>':"\n");
+  echo "Ran every-time functions".$ASBreak;
 }
 
 //-------------- Run every hour ------------------------------------------//
 if ($Hour != $NextHour || (isset($_GET['runhour']) && $_GET['runhour'])) {
   run_all_in('hourly');
-  echo "Ran hourly functions".($AS?'<br>':"\n");
+  echo "Ran hourly functions".$ASBreak;
 }
 
 //-------------- Run every day -------------------------------------------//
 if ($Day != $NextDay || (isset($_GET['runday']) && $_GET['runday'])) {
   run_all_in('daily');
-  echo "Ran daily functions".($AS?'<br>':"\n");
+  echo "Ran daily functions".$ASBreak;
 }
 
 //-------------- Run every week -------------------------------------------//
 if (($Day != $NextDay && date('w') == 0) || (isset($_GET['runweek']) && $_GET['runweek'])) {
   run_all_in('weekly');
-  echo "Ran weekly functions".($AS?'<br>':"\n");
+  echo "Ran weekly functions".$ASBreak;
 }
 
 //--------------- Run twice per month -------------------------------------//
 if ($BiWeek != $NextBiWeek || (isset($_GET['runbiweek']) && $_GET['runbiweek'])) {
   run_all_in('biweekly');
-  echo "Ran bi-weekly functions".($AS?'<br>':"\n");
+  echo "Ran bi-weekly functions".$ASBreak;
 }
 
 //---------------- Run every month -----------------------------------------//
 if (($BiWeek != $NextBiWeek && $BiWeek == 8) || (isset($_GET['runmonth']) && $_GET['runmonth'])) {
   run_all_in('monthly');
-  echo "Ran monthly functions".($AS?'<br>':"\n");
+  echo "Ran monthly functions".$ASBreak;
 }
 
 //---------------- Run on request ------------------------------------------//
 if (isset($_GET['runmanual']) && $_GET['runmanual']) {
   run_all_in('manually');
-  echo "Ran manual functions".($AS?'<br>':"\n");
+  echo "Ran manual functions".$ASBreak;
 }
 
 if ($AS) {
