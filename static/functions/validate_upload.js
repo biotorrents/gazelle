8dad3df9b6929cbb172cb7ff2858268782921bc9
spaghetti <spaghetti@oppaiti.me>
2018-02-17 13:20:10 -0500

    Encourage uploaders to think twice about 1080p DVDs

--- static/functions/validate_upload.js
+++ static/functions/validate_upload.js
@@ -18,6 +18,7 @@ $(() => {
     if ($('#album_desc').raw().value.length < 10) {
       hard_error('The group description is too short')
     }
+
     if ($('#file').raw().value.slice(-8).toLowerCase() != '.torrent') {
       soft_error('The file selected does not appear to be a .torrent file')
     }
@@ -27,6 +28,9 @@ $(() => {
     }
     if (!$('#image').raw().value) {
       soft_error('You did not include a cover image, which is mandatory if one exists')
+    }
+    if ($('#media_tr select').raw().value == 'DVD' && (['720p','1080i','1080p','4K'].includes($('#ressel').raw().value) || +$('#resolution').raw().value.split('x')[1] > 576)) {
+      soft_error('Your selected resolution is too high to be a DVD. Are you sure the media type should be DVD and not WEB?')
     }
   })
   $('#tags').on('blur', e => $('#tags').raw().value = $('#tags').raw().value.split(',').map(t=>t.trim()).filter(t=>t).join(', '))
