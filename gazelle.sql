f9846756afdca55ef6fdc2ba94a83cefb544611d
pjc <desk@pjc.is>
2019-11-21 17:42:50 +0100

    Fix Ocelot query error in `xbt_files_users`

--- gazelle.sql
+++ gazelle.sql
@@ -1732,7 +1732,7 @@ CREATE TABLE `xbt_files_users` (
   `announced` int(11) NOT NULL,
   `completed` tinyint(1) NOT NULL DEFAULT '0',
   `downloaded` bigint(20) NOT NULL DEFAULT '0',
-  `remaining` bigint(20) NOT NULL,
+  `remaining` bigint(20) NOT NULL DEFAULT '0',
   `uploaded` bigint(20) NOT NULL,
   `upspeed` int(10) unsigned NOT NULL,
   `downspeed` int(10) unsigned NOT NULL,
