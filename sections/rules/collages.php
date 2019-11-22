91af3ffed8901a512a58b184b3d348457f8d40fc
pjc <desk@pjc.is>
2019-11-22 17:40:54 +0100

    Titty Monsters

--- sections/rules/collages.php
+++ sections/rules/collages.php
@@ -12,10 +12,7 @@ View::show_header('Collection Rules');
       Collection vandalism is taken very seriously, resulting in collection editing privileges being removed (at a minimum).
       </li>
       <li>
-      Personal Best Of collections are only allowed in the new Personal Collections category. You must be a Well Endowed+ or Donor to create one.
-      </li>
-      <li>
-      A well-defined group of people, for instance Titty Monsters, or Interviewers, may create a Group Picks Theme collection with one pick per person, after having gained permission for the collection from Staff.
+      A well-defined group of people, for instance Counter Culture Labs members, may create a Group Picks Theme collection with one pick per person, after having gained permission for the collection from Staff.
       </li>
       <li>
       There may only be one collection per Theme. Dupe collections will be deleted. The collection that is allowed to stay will be determined at staff discretion, with priority given to the most well maintained and oldest collection.
@@ -24,16 +21,13 @@ View::show_header('Collection Rules');
       Theme collections must be sensible, and reasonably broad. Those that do not fit this description will be deleted.
       </li>
       <li>
-      Collections are <strong>not</strong> an alternative to the tagging system. A collection such as 'lolicon torrents' wouldn't be allowed, because it is far more appropriate to just tag the torrents as lolicon. Of course, an 'xysite.com best 30 lolicon eroge' collection would be looked upon differently.
+      Collections are <strong>not</strong> an alternative to the tagging system. A collection such as "fungi torrents" wouldn't be allowed, because it is far more appropriate to just tag the torrents as fungi. Of course, a "fungi rRNA barcodes" collection would be looked upon differently.
       </li>
       <li>
       Collections should not be used to create lists of an artist's works, as the artist pages already exist for this purpose.
       </li>
       <li>
-      Well Endowed and Donors get one personal collection. Bombshells can have two, Top Heavies can have up to three, and Titty Monsters up to four. Donors always receive one more than the class maximum.
-      </li>
-      <li>
-      Every collection must have at least 3 torrent groups in it, except for collections of type "Personal", and "Staff Picks".
+      Every collection must have at least 3 torrent groups in it, except for collections of type "Personal" and "Staff Picks."
       </li>
       <li>
       Check to see that a similar collection does not already exist. If a similar collection does exist, please contribute to the existing collection.
@@ -44,7 +38,6 @@ View::show_header('Collection Rules');
       <li>
       Attempt to add a cover image to every torrent in your collection.
       </li>
-
     </ul>
   </div>
 <? include('jump.php'); ?>
