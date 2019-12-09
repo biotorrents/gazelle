59a8eb2e596661bf8bec0fae7715ee40295295c1
pjc <desk@pjc.is>
2019-12-09 18:14:54 +0100

    Update torrent group edit form

--- sections/torrents/editgroup.php
+++ sections/torrents/editgroup.php
@@ -1,4 +1,5 @@
-<?
+<?php
+
 /************************************************************************
 ||------------|| Edit torrent group wiki page ||-----------------------||
 
@@ -15,7 +16,7 @@ the cache for the torrent group page.
 
 $GroupID = $_GET['groupid'];
 if (!is_number($GroupID) || !$GroupID) {
-  error(0);
+    error(0);
 }
 
 // Get the torrent group name and the body of the last revision
@@ -40,7 +41,7 @@ $DB->query("
     LEFT JOIN wiki_torrents AS wt ON wt.RevisionID = tg.RevisionID
   WHERE tg.ID = '".db_string($GroupID)."'");
 if (!$DB->has_results()) {
-  error(404);
+    error(404);
 }
 list($Name, $NameRJ, $NameJP, $Image, $Body, $WikiImage, $WikiBody, $Year, $Studio, $Series, $DLsiteID, $CatalogueNumber, $Pages, $CategoryID, $DLsiteID) = $DB->next_record();
 
@@ -51,17 +52,17 @@ $DB->query("
   WHERE GroupID = '".db_string($GroupID)."'");
 
 if ($DB->has_results()) {
-  $Screenshots = [];
-  while ($S = $DB->next_record(MYSQLI_ASSOC, true)) {
-    $Screenshots[] = $S;
-  }
+    $Screenshots = [];
+    while ($S = $DB->next_record(MYSQLI_ASSOC, true)) {
+        $Screenshots[] = $S;
+    }
 }
 
 $Artists = Artists::get_artists(array($GroupID))[$GroupID];
 
 if (!$Body) {
-  $Body = $WikiBody;
-  $Image = $WikiImage;
+    $Body = $WikiBody;
+    $Image = $WikiImage;
 }
 
 View::show_header('Edit torrent group', 'upload,bbcode');
@@ -76,13 +77,16 @@ View::show_header('Edit torrent group', 'upload,bbcode');
     <form class="edit_form" name="torrent_group" action="torrents.php" method="post">
       <div>
         <input type="hidden" name="action" value="takegroupedit" />
-        <input type="hidden" name="auth" value="<?=$LoggedUser['AuthKey']?>" />
+        <input type="hidden" name="auth"
+          value="<?=$LoggedUser['AuthKey']?>" />
         <input type="hidden" name="groupid" value="<?=$GroupID?>" />
-        <h3>Image:</h3>
-        <input type="text" name="image" size="92" value="<?=$Image?>" /><br />
-        <h3>Torrent group description:</h3>
-        <textarea class="bbcode_editor" name="body" cols="91" rows="20"><?=$Body?></textarea><br />
-<?
+        <h3>Picture</h3>
+        <input type="text" name="image" size="92"
+          value="<?=$Image?>" /><br /><br />
+        <h3>Torrent Group Descriptions</h3>
+        <textarea class="bbcode_editor" name="body" cols="91"
+          rows="20"><?=$Body?></textarea><br /><br />
+        <?php
   $DB->query("
     SELECT UserID
     FROM torrents
@@ -90,7 +94,7 @@ View::show_header('Edit torrent group', 'upload,bbcode');
 
   $Contributed = in_array($LoggedUser['ID'], $DB->collect('UserID'));
 ?>
-        <h3>Edit summary:</h3>
+        <h3>Edit Summary</h3>
         <input type="text" name="summary" size="92" /><br />
         <div style="text-align: center;">
           <input type="submit" value="Submit" />
@@ -98,46 +102,61 @@ View::show_header('Edit torrent group', 'upload,bbcode');
       </div>
     </form>
   </div>
-<?
+  <?php
   if ($Contributed || check_perms('torrents_edit') || check_perms('screenshots_delete') || check_perms('screenshots_add')) {
-?>
-  <h3 id="screenshots_section"><?=($CategoryID == 3)?'Samples':'Screenshots'?></h3>
+      ?>
+  <h3 id="screenshots_section">Publications</h3>
   <div class="box pad">
-    <p><strong class="important_text">Thumbs, promotional material, and preview images consisting of multiple images are not allowed as screenshots.</strong></p>
+    <!-- <p><strong class="important_text">Thumbs, promotional material, and preview images consisting of multiple images are not allowed as screenshots.</strong></p> -->
     <form class="edit_form" name="screenshots_form" action="torrents.php" method="post">
       <input type="hidden" name="action" value="screenshotedit" />
-      <input type="hidden" name="auth" value="<?=$LoggedUser['AuthKey']?>" />
+      <input type="hidden" name="auth"
+        value="<?=$LoggedUser['AuthKey']?>" />
       <input type="hidden" name="groupid" value="<?=$GroupID?>" />
       <table cellpadding="3" cellspacing="1" border="0" class="layout" width="100%">
         <tr>
-          <td class="label"><?=($CategoryID == 3)?'Samples':'Screenshots'?>:</td>
+          <td class="label">Publications</td>
           <td id="screenshots">
-<?
+            <?php
   if ($Contributed || check_perms('screenshots_add') || check_perms('torrents_edit')) {
-?>
-          <a class="float_right brackets" onclick="AddScreenshotField()">+</a>
-<?
+      ?>
+            <a class="float_right brackets" onclick="AddScreenshotField()">+</a>
+            <?php
   }
 
-  foreach($Screenshots as $i => $Screenshot) {
-    $SSURL = ImageTools::process($Screenshot['Image'], 'thumb');
-?>
+      foreach ($Screenshots as $i => $Screenshot) {
+          /* Image host integration
+          $SSURL = ImageTools::process($Screenshot['Image'], 'thumb');
+          */ ?>
             <div>
-              <input type="text" size="45" id="ss_<?=$i?>" name="screenshots[]" value="<?=$Screenshot['Image']?>"/>
-<?
+              <input type="text" size="45" id="ss_<?=$i?>"
+                name="screenshots[]"
+                value="<?=$Screenshot['Image']?>" />
+              <?php
   if ($Screenshot['UserID'] == $LoggedUser['ID'] || check_perms('torrents_edit') || check_perms('screenshots_delete')) {
-?>
+      ?>
               <a onclick="RemoveScreenshotField(this)" class="brackets">&minus;</a>
-<? } ?>
+              <?php
+  } ?>
               <br />
-<?            if (check_perms('users_mod')) { ?>
-                <img class="tooltip lightbox-init" title='<?=Users::format_username($Screenshot['UserID'], false, false, false)?> - <?=time_diff($Screenshot['Time'])?>' src="<?=$SSURL?>" />
-<?            } else { ?>
-                <img class="tooltip lightbox-init" title='Added <?=time_diff($Screenshot['Time'])?>' src="<?=$SSURL?>" />
-<?            } ?>
+              <?php if (check_perms('users_mod')) { ?>
+              <!--
+              <img class="tooltip lightbox-init"
+                title='<?=Users::format_username($Screenshot['UserID'], false, false, false)?>
+              - <?=time_diff($Screenshot['Time'])?>'
+              src="<?=$SSURL?>" />
+              -->
+              <?php } else { ?>
+              <!--
+              <img class="tooltip lightbox-init"
+                title='Added <?=time_diff($Screenshot['Time'])?>'
+              src="<?=$SSURL?>" />
+              -->
+              <?php } ?>
             </div>
             <br />
-<? } ?>
+            <?php
+      } ?>
           </td>
         </tr>
       </table>
@@ -146,19 +165,22 @@ View::show_header('Edit torrent group', 'upload,bbcode');
       </div>
     </form>
   </div>
-<? }
+  <?php
+  }
   //Users can edit the group info if they've uploaded a torrent to the group or have torrents_edit
   if ($Contributed || check_perms('torrents_edit')) { ?>
   <h3>Non-wiki torrent group editing</h3>
   <div class="box pad">
     <form class="edit_form" name="torrent_group" action="torrents.php" method="post">
       <input type="hidden" name="action" value="nonwikiedit" />
-      <input type="hidden" name="auth" value="<?=$LoggedUser['AuthKey']?>" />
+      <input type="hidden" name="auth"
+        value="<?=$LoggedUser['AuthKey']?>" />
       <input type="hidden" name="groupid" value="<?=$GroupID?>" />
       <table cellpadding="3" cellspacing="1" border="0" class="layout" width="100%">
-      <tr>
-        <td class="label">
-<?
+        <tr>
+          <td class="label">Author(s)
+            <?php
+/*
 if ($CategoryID == 1) {
   echo "Idol(s):";
 } elseif ($CategoryID == 2) {
@@ -170,124 +192,99 @@ if ($CategoryID == 1) {
 } elseif ($CategoryID == 5) {
   echo "Creator/Author:";
 }
+*/
 ?>
-        </td>
-        <td id="idolfields">
-          <input type="text" id="idol_0" name="idols[]" size="45" value="<?=$Artists[0]['name']?>"/>
-          <a class="add_artist_button brackets">+</a> <a class="remove_artist_button brackets">&minus;</a>
-<?
+          </td>
+          <td id="idolfields">
+            <input type="text" id="idol_0" name="idols[]" size="45"
+              value="<?=$Artists[0]['name']?>" />
+            <a class="add_artist_button brackets">+</a> <a class="remove_artist_button brackets">&minus;</a>
+            <?php
   for ($i = 1; $i < count($Artists); $i++) {
-    print '<br><input type="text" id="idol_'.$i.'" name="idols[]" size="45" value="'.$Artists[$i]['name'].'"/>';
-  }
-?>
-        </td>
-      </tr>
-<? if ($CategoryID != 2) { ?>
-        <tr>
-          <td class="label">
-<? if ($CategoryID == 1) {
-     echo "Studio:";
-   } else {
-     echo "Publisher:";
-   }
-?>
-          </td>
-          <td><input type="text" id="studio" name="studio" size="60" value="<?=$Studio?>" /></td>
-        </tr>
-<? }
-if ($CategoryID != 5) { ?>
-        <tr>
-          <td class="label">
-<?
-  if ($CategoryID == 1) {
-    echo "Series:";
-  } else {
-    echo "Circle:";
+      print '<br><input type="text" id="idol_'.$i.'" name="idols[]" size="45" value="'.$Artists[$i]['name'].'"/>';
   }
 ?>
           </td>
-          <td><input type="text" id="series" name="series" size="60" value="<?=$Series?>"/></td>
-<? } ?>
-<? if ($CategoryID != 5) { ?>
+        </tr>
         <tr>
-          <td class="label">Year:</td>
+          <td class="label">Department/Lab</td>
+          <td><input type="text" id="studio" name="studio" size="60"
+              value="<?=$Studio?>" /></td>
+        </tr>
+        <tr>
+          <td class="label">Location</td>
+          <td><input type="text" id="series" name="series" size="60"
+              value="<?=$Series?>" /></td>
+        <tr>
+          <td class="label">Year</td>
           <td>
-            <input type="text" name="year" size="10" value="<?=$Year?>" />
+            <input type="text" name="year" size="10"
+              value="<?=$Year?>" />
           </td>
         </tr>
-<? } ?>
-<? if ($CategoryID == 3) { ?>
         <tr>
-          <td class="label">Pages:</td>
+          <td class="label">Accession Number</td>
           <td>
-            <input type="text" name="pages" size="10" value="<?=$Pages?>" />
+            <input type="text" name="catalogue" size="40"
+              value="<?=$CatalogueNumber?>" />
           </td>
         </tr>
-<? } ?>
-<? if ($CategoryID == 4 || $CategoryID == 5) { ?>
-        <tr>
-          <td class="label">DLsite ID:</td>
-          <td><input type="text" id="dlsiteid" name="dlsiteid" size="8" maxlength="8" value="<?=$DLsiteID?>"/></td>
-        </tr>
-<? } ?>
-<?
-  if ($CategoryID == 1) { ?>
-        <tr>
-          <td class="label">Catalogue Number:</td>
-          <td>
-            <input type="text" name="catalogue" size="40" value="<?=$CatalogueNumber?>" />
-          </td>
-        </tr>
-<? } ?>
-<?  if (check_perms('torrents_freeleech')) { ?>
+        <?php  if (check_perms('torrents_freeleech')) { ?>
         <tr>
           <td class="label">Torrent <strong>group</strong> leech status</td>
           <td>
             <input type="checkbox" id="unfreeleech" name="unfreeleech" /><label for="unfreeleech"> Reset</label>
             <input type="checkbox" id="freeleech" name="freeleech" /><label for="freeleech"> Freeleech</label>
-            <input type="checkbox" id="neutralleech" name="neutralleech" /><label for="neutralleech"> Neutral Leech</label>
-             because
+            <input type="checkbox" id="neutralleech" name="neutralleech" /><label for="neutralleech"> Neutral
+              Leech</label>
+            because
             <select name="freeleechtype">
-<?    $FL = array('N/A', 'Staff Pick', 'Perma-FL', 'Freeleechizer', 'Site-Wide FL');
+              <?php    $FL = array('N/A', 'Staff Pick', 'Perma-FL', 'Freeleechizer', 'Site-Wide FL');
     foreach ($FL as $Key => $FLType) { ?>
-              <option value="<?=$Key?>"<?=($Key == $Torrent['FreeLeechType'] ? ' selected="selected"' : '')?>><?=$FLType?></option>
-<?    } ?>
+              <option value="<?=$Key?>" <?=($Key == $Torrent['FreeLeechType'] ? ' selected="selected"' : '')?>><?=$FLType?>
+              </option>
+              <?php    } ?>
             </select>
           </td>
         </tr>
-<?  } ?>
+        <?php  } ?>
       </table>
       <input type="submit" value="Edit" />
     </form>
   </div>
-<?
+  <?php
   }
   if ($Contributed || check_perms('torrents_edit')) {
-?>
+      ?>
   <h3>Rename (will not merge)</h3>
   <div class="box pad">
     <form class="rename_form" name="torrent_group" action="torrents.php" method="post">
       <div>
         <table cellpadding="3" cellspacing="1" border="0" class="layout" width="100%">
           <input type="hidden" name="action" value="rename" />
-          <input type="hidden" name="auth" value="<?=$LoggedUser['AuthKey']?>" />
-          <input type="hidden" name="groupid" value="<?=$GroupID?>" />
+          <input type="hidden" name="auth"
+            value="<?=$LoggedUser['AuthKey']?>" />
+          <input type="hidden" name="groupid"
+            value="<?=$GroupID?>" />
           <tr>
-            <td class="label">English Title: </td>
+            <td class="label">Torrent Title</td>
             <td>
-              <input type="text" name="name" size="70" value="<?=$Name?>" />
+              <input type="text" name="name" size="70"
+                value="<?=$Name?>" />
             </td>
           </tr>
           <tr>
-            <td class="label">Romaji Title: </td>
+            <td class="label">Organism</td>
             <td>
-              <input type="text" name="namerj" size="70" value="<?=$NameRJ?>" />
+              <input type="text" name="namerj" size="70"
+                value="<?=$NameRJ?>" />
             </td>
           </tr>
           <tr>
-            <td class="label">Japanese Title: </td>
+            <td class="label">Strain/Variety</td>
             <td>
-              <input type="text" name="namejp" size="70" value="<?=$NameJP?>" />
+              <input type="text" name="namejp" size="70"
+                value="<?=$NameJP?>" />
             </td>
           </tr>
         </table>
@@ -297,16 +294,18 @@ if ($CategoryID != 5) { ?>
       </div>
     </form>
   </div>
-<? }
+  <?php
+  }
   if (check_perms('torrents_edit')) { ?>
   <h3>Merge with another group</h3>
   <div class="box pad">
     <form class="merge_form" name="torrent_group" action="torrents.php" method="post">
       <div>
         <input type="hidden" name="action" value="merge" />
-        <input type="hidden" name="auth" value="<?=$LoggedUser['AuthKey']?>" />
+        <input type="hidden" name="auth"
+          value="<?=$LoggedUser['AuthKey']?>" />
         <input type="hidden" name="groupid" value="<?=$GroupID?>" />
-        <h3>Target torrent group ID:
+        <h3>Target torrent group ID
           <input type="text" name="targetgroupid" size="10" />
         </h3>
         <div style="text-align: center;">
@@ -315,6 +314,6 @@ if ($CategoryID != 5) { ?>
       </div>
     </form>
   </div>
-<?  } ?>
+  <?php  } ?>
 </div>
-<? View::show_footer(); ?>
+<?php View::show_footer();
