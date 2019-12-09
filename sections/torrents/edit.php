9208c6c3d97f2ee6a5b1dc14c1fba10ea5e16940
pjc <desk@pjc.is>
2019-12-09 18:20:43 +0100

    Update individual torrent edit form

--- sections/torrents/edit.php
+++ sections/torrents/edit.php
@@ -1,4 +1,5 @@
-<?
+<?php
+
 //**********************************************************************//
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Edit form ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 // This page relies on the TorrentForm class. All it does is call      //
@@ -11,7 +12,7 @@
 require_once(SERVER_ROOT.'/classes/torrent_form.class.php');
 
 if (!is_number($_GET['id']) || !$_GET['id']) {
-  error(0);
+    error(0);
 }
 
 $TorrentID = $_GET['id'];
@@ -56,13 +57,13 @@ $DB->query("
 
 list($Properties) = $DB->to_array(false, MYSQLI_BOTH);
 if (!$Properties) {
-  error(404);
+    error(404);
 }
 
 $UploadForm = $Categories[$Properties['CategoryID'] - 1];
 
 if (($LoggedUser['ID'] != $Properties['UserID'] && !check_perms('torrents_edit')) || $LoggedUser['DisableWiki']) {
-  error(403);
+    error(403);
 }
 
 View::show_header('Edit torrent', 'upload,torrent,bbcode');
@@ -72,139 +73,169 @@ $TorrentForm = new TorrentForm($Properties, $Err, false);
 $TorrentForm->upload_form();
 
 if (check_perms('torrents_edit') || check_perms('users_mod')) {
-?>
+    ?>
 <div class="thin">
-<?
+  <?php
   if ($Properties['CategoryID'] != 5) {
-?>
+      ?>
   <div class="header">
-    <h2>Change group</h2>
+    <h2>Change Group</h2>
   </div>
   <div class="box pad">
-  <form class="edit_form" name="torrent_group" action="torrents.php" method="post">
-    <input type="hidden" name="action" value="editgroupid" />
-    <input type="hidden" name="auth" value="<?=$LoggedUser['AuthKey']?>" />
-    <input type="hidden" name="torrentid" value="<?=$TorrentID?>" />
-    <input type="hidden" name="oldgroupid" value="<?=$Properties['GroupID']?>" />
-    <table class="layout">
-      <tr>
-        <td class="label">Group ID</td>
-        <td>
-          <input type="text" name="groupid" value="<?=$Properties['GroupID']?>" size="10" />
-        </td>
-      </tr>
-      <tr>
-        <td colspan="2" class="center">
-          <input type="submit" value="Change group ID" />
-        </td>
-      </tr>
-    </table>
-  </form>
+    <form class="edit_form" name="torrent_group" action="torrents.php" method="post">
+      <input type="hidden" name="action" value="editgroupid" />
+      <input type="hidden" name="auth"
+        value="<?=$LoggedUser['AuthKey']?>" />
+      <input type="hidden" name="torrentid"
+        value="<?=$TorrentID?>" />
+      <input type="hidden" name="oldgroupid"
+        value="<?=$Properties['GroupID']?>" />
+      <table class="layout">
+        <tr>
+          <td class="label">Group ID</td>
+          <td>
+            <input type="text" name="groupid"
+              value="<?=$Properties['GroupID']?>"
+              size="10" />
+          </td>
+        </tr>
+        <tr>
+          <td colspan="2" class="center">
+            <input type="submit" value="Change Group ID" />
+          </td>
+        </tr>
+      </table>
+    </form>
   </div>
   <h2>Split off into new group</h2>
   <div class="box pad">
-  <form class="split_form" name="torrent_group" action="torrents.php" method="post">
-    <input type="hidden" name="action" value="newgroup" />
-    <input type="hidden" name="auth" value="<?=$LoggedUser['AuthKey']?>" />
-    <input type="hidden" name="torrentid" value="<?=$TorrentID?>" />
-    <input type="hidden" name="oldgroupid" value="<?=$Properties['GroupID']?>" />
-    <table class="layout">
-      <tr>
-        <td class="label">Artist</td>
-        <td>
-          <input type="text" name="artist" value="<?=$Properties['ArtistName']?>" size="50" />
-        </td>
-      </tr>
-      <tr>
-        <td class="label">Title</td>
-        <td>
-          <input type="text" name="title" value="<?=$Properties['Title']?>" size="50" />
-        </td>
-      </tr>
-      <tr>
-        <td class="label">Japanese Title</td>
-        <td>
-          <input type="test" name="title_jp" value="<?=$Properties['TitleJP']?>" size=50" />
-        </td>
-      <tr>
-        <td class="label">Year</td>
-        <td>
-          <input type="text" name="year" value="<?=$Properties['Year']?>" size="10" />
-        </td>
-      </tr>
-      <tr>
-        <td colspan="2" class="center">
-          <input type="submit" value="Split into new group" />
-        </td>
-      </tr>
-    </table>
-  </form>
+    <form class="split_form" name="torrent_group" action="torrents.php" method="post">
+      <input type="hidden" name="action" value="newgroup" />
+      <input type="hidden" name="auth"
+        value="<?=$LoggedUser['AuthKey']?>" />
+      <input type="hidden" name="torrentid"
+        value="<?=$TorrentID?>" />
+      <input type="hidden" name="oldgroupid"
+        value="<?=$Properties['GroupID']?>" />
+      <table class="layout">
+        <tr>
+          <td class="label">Author</td>
+          <td>
+            <input type="text" name="artist"
+              value="<?=$Properties['ArtistName']?>"
+              size="50" />
+          </td>
+        </tr>
+        <tr>
+          <td class="label">Torrent Title</td>
+          <td>
+            <input type="text" name="title"
+              value="<?=$Properties['Title']?>"
+              size="50" />
+          </td>
+        </tr>
+        <tr>
+          <td class="label">Strain/Variety</td>
+          <td>
+            <input type="test" name="title_jp"
+              value="<?=$Properties['TitleJP']?>"
+              size=50" />
+          </td>
+        <tr>
+          <td class="label">Year</td>
+          <td>
+            <input type="text" name="year"
+              value="<?=$Properties['Year']?>"
+              size="10" />
+          </td>
+        </tr>
+        <tr>
+          <td colspan="2" class="center">
+            <input type="submit" value="Split into new group" />
+          </td>
+        </tr>
+      </table>
+    </form>
   </div>
   <br />
-<?
+  <?php
   }
-  if (check_perms('users_mod')) { ?>
-  <h2>Change category</h2>
+    if (check_perms('users_mod')) { ?>
+  <h2>Change Category</h2>
   <div class="box pad">
-  <form action="torrents.php" method="post">
-    <input type="hidden" name="action" value="changecategory" />
-    <input type="hidden" name="auth" value="<?=$LoggedUser['AuthKey']?>" />
-    <input type="hidden" name="torrentid" value="<?=$TorrentID?>" />
-    <input type="hidden" name="oldgroupid" value="<?=$Properties['GroupID']?>" />
-    <input type="hidden" name="oldartistid" value="<?=$Properties['ArtistID']?>" />
-    <input type="hidden" name="oldcategoryid" value="<?=$Properties['CategoryID']?>" />
-    <table>
-      <tr>
-        <td class="label">Change category</td>
-        <td>
-          <select id="newcategoryid" name="newcategoryid">
-<?    foreach ($Categories as $CatID => $CatName) { ?>
-            <option value="<?=($CatID + 1)?>"<?Format::selected('CategoryID', $CatID + 1, 'selected', $Properties)?>><?=($CatName)?></option>
-<?    } ?>
-          </select>
-        </td>
-      <tr id="split_releasetype">
-        <td class="label">Release type</td>
-        <td>
-          <select name="releasetype">
-<?    foreach ($ReleaseTypes as $RTID => $ReleaseType) { ?>
-            <option value="<?=($RTID)?>"><?=($ReleaseType)?></option>
-<?    } ?>
-          </select>
-        </td>
-      </tr>
-      <tr id="split_artist">
-        <td class="label">Artist</td>
-        <td>
-          <input type="text" name="artist" value="<?=$Properties['ArtistName']?>" size="50" />
-        </td>
-      </tr>
-      <tr id="split_title">
-        <td class="label">Title</td>
-        <td>
-          <input type="text" name="title" value="<?=$Properties['Title']?>" size="50" />
-        </td>
-      </tr>
-      <tr id="split_year">
-        <td class="label">Year</td>
-        <td>
-          <input type="text" name="year" value="<?=$Properties['Year']?>" size="10" />
-        </td>
-      </tr>
-      <tr>
-        <td colspan="2" class="center">
-          <input type="submit" value="Change category" />
-        </td>
-      </tr>
-    </table>
-    <script type="text/javascript">ChangeCategory($('#newcategoryid').raw().value);</script>
-  </form>
+    <form action="torrents.php" method="post">
+      <input type="hidden" name="action" value="changecategory" />
+      <input type="hidden" name="auth"
+        value="<?=$LoggedUser['AuthKey']?>" />
+      <input type="hidden" name="torrentid"
+        value="<?=$TorrentID?>" />
+      <input type="hidden" name="oldgroupid"
+        value="<?=$Properties['GroupID']?>" />
+      <input type="hidden" name="oldartistid"
+        value="<?=$Properties['ArtistID']?>" />
+      <input type="hidden" name="oldcategoryid"
+        value="<?=$Properties['CategoryID']?>" />
+      <table>
+        <tr>
+          <td class="label">Category</td>
+          <td>
+            <select id="newcategoryid" name="newcategoryid">
+              <?php    foreach ($Categories as $CatID => $CatName) { ?>
+              <option value="<?=($CatID + 1)?>" <?Format::selected('CategoryID', $CatID + 1, 'selected', $Properties)?>><?=($CatName)?>
+              </option>
+              <?php    } ?>
+            </select>
+          </td>
+        <tr id="split_releasetype">
+          <td class="label">Release type</td>
+          <td>
+            <select name="releasetype">
+              <?php    foreach ($ReleaseTypes as $RTID => $ReleaseType) { ?>
+              <option value="<?=($RTID)?>"><?=($ReleaseType)?>
+              </option>
+              <?php    } ?>
+            </select>
+          </td>
+        </tr>
+        <tr id="split_artist">
+          <td class="label">Artist</td>
+          <td>
+            <input type="text" name="artist"
+              value="<?=$Properties['ArtistName']?>"
+              size="50" />
+          </td>
+        </tr>
+        <tr id="split_title">
+          <td class="label">Torrent Title</td>
+          <td>
+            <input type="text" name="title"
+              value="<?=$Properties['Title']?>"
+              size="50" />
+          </td>
+        </tr>
+        <tr id="split_year">
+          <td class="label">Year</td>
+          <td>
+            <input type="text" name="year"
+              value="<?=$Properties['Year']?>"
+              size="10" />
+          </td>
+        </tr>
+        <tr>
+          <td colspan="2" class="center">
+            <input type="submit" value="Change Category" />
+          </td>
+        </tr>
+      </table>
+      <script type="text/javascript">
+        ChangeCategory($('#newcategoryid').raw().value);
+      </script>
+    </form>
   </div>
-<?
-  }
-?>
+  <?php
+  } ?>
 </div>
-<?
+<?php
 } // if check_perms('torrents_edit')
 
-View::show_footer(); ?>
+View::show_footer();
