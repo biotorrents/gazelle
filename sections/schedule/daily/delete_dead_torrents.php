8b90471234e5cb64fd3231aba203ee48a9af0450
pjc <desk@pjc.is>
2020-01-15 18:16:03 +0100

    Disable inactivity deletions for now

--- sections/schedule/daily/delete_dead_torrents.php
+++ sections/schedule/daily/delete_dead_torrents.php
@@ -1,6 +1,7 @@
-<?
+<?php
 //------------- Delete dead torrents ------------------------------------//
 
+/*
 $DB->query("
   SELECT
     t.ID,
@@ -21,47 +22,47 @@ echo 'Found '.count($Torrents)." inactive torrents to be deleted.\n";
 $LogEntries = $DeleteNotes = [];
 
 // Exceptions for inactivity deletion
-$InactivityExceptionsMade = []; //UserID => expiry time of exception
+$InactivityExceptionsMade = []; // UserID => expiry time of exception
 
 $i = 0;
 foreach ($Torrents as $Torrent) {
-  list($ID, $GroupID, $Name, $UserID, $Media, $InfoHash) = $Torrent;
-  if (array_key_exists($UserID, $InactivityExceptionsMade) && (time() < $InactivityExceptionsMade[$UserID])) {
-    // don't delete the torrent!
-    continue;
-  }
-  $ArtistName = Artists::display_artists(Artists::get_artist($GroupID), false, false, false);
-  if ($ArtistName) {
-    $Name = "$ArtistName - $Name";
-  }
-  Torrents::delete_torrent($ID, $GroupID);
-  $LogEntries[] = db_string("Torrent $ID ($Name) (".strtoupper($InfoHash).") was deleted for inactivity (unseeded)");
+    list($ID, $GroupID, $Name, $UserID, $Media, $InfoHash) = $Torrent;
+    if (array_key_exists($UserID, $InactivityExceptionsMade) && (time() < $InactivityExceptionsMade[$UserID])) {
+        // Don't delete the torrent!
+        continue;
+    }
+    $ArtistName = Artists::display_artists(Artists::get_artist($GroupID), false, false, false);
+    if ($ArtistName) {
+        $Name = "$ArtistName - $Name";
+    }
+    Torrents::delete_torrent($ID, $GroupID);
+    $LogEntries[] = db_string("Torrent $ID ($Name) (".strtoupper($InfoHash).") was deleted for inactivity (unseeded)");
 
-  if (!array_key_exists($UserID, $DeleteNotes)) {
-    $DeleteNotes[$UserID] = array('Count' => 0, 'Msg' => '');
-  }
+    if (!array_key_exists($UserID, $DeleteNotes)) {
+        $DeleteNotes[$UserID] = array('Count' => 0, 'Msg' => '');
+    }
 
-  $DeleteNotes[$UserID]['Msg'] .= "\n$Name";
-  $DeleteNotes[$UserID]['Count']++;
+    $DeleteNotes[$UserID]['Msg'] .= "\n$Name";
+    $DeleteNotes[$UserID]['Count']++;
 
-  ++$i;
-  if ($i % 500 == 0) {
-    echo "$i inactive torrents removed.\n";
-  }
+    ++$i;
+    if ($i % 500 === 0) {
+        echo "$i inactive torrents removed.\n";
+    }
 }
 echo "$i torrents deleted for inactivity.\n";
 
 foreach ($DeleteNotes as $UserID => $MessageInfo) {
-  $Singular = ($MessageInfo['Count'] == 1);
-  Misc::send_pm($UserID, 0, $MessageInfo['Count'].' of your torrents '.($Singular ? 'has' : 'have').' been deleted for inactivity', ($Singular ? 'One' : 'Some').' of your uploads '.($Singular ? 'has' : 'have').' been deleted for being unseeded. Since '.($Singular ? 'it' : 'they').' didn\'t break any rules (we hope), please feel free to re-upload '.($Singular ? 'it' : 'them').".\n\nThe following torrent".($Singular ? ' was' : 's were').' deleted:'.$MessageInfo['Msg']);
+    $Singular = ($MessageInfo['Count'] === 1);
+    Misc::send_pm($UserID, 0, $MessageInfo['Count'].' of your torrents '.($Singular ? 'has' : 'have').' been deleted for inactivity', ($Singular ? 'One' : 'Some').' of your uploads '.($Singular ? 'has' : 'have').' been deleted for being unseeded. Since '.($Singular ? 'it' : 'they').' didn\'t break any rules (we hope), please feel free to re-upload '.($Singular ? 'it' : 'them').".\n\nThe following torrent".($Singular ? ' was' : 's were').' deleted:'.$MessageInfo['Msg']);
 }
 unset($DeleteNotes);
 
 if (count($LogEntries) > 0) {
-  $Values = "('".implode("', '$sqltime'), ('", $LogEntries) . "', '$sqltime')";
-  $DB->query("
+    $Values = "('".implode("', '$sqltime'), ('", $LogEntries) . "', '$sqltime')";
+    $DB->query("
     INSERT INTO log (Message, Time)
     VALUES $Values");
-  echo "\nDeleted $i torrents for inactivity\n";
+    echo "\nDeleted $i torrents for inactivity\n";
 }
-?>
+*/
