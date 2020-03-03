09c426cde0f9a0ea2dc8f18ca0c459cdd07d52dd
pjc <desk@pjc.is>
2020-03-03 20:26:59 +0100

    Package the BioTorrents.de default theme

---
+++ _packages/matcha-theme/gazelle.sql
@@ -0,0 +1,3 @@
+-- Line 1791
+INSERT INTO stylesheets (ID, Name, Description, `Default`, Additions, Color) VALUES (1, 'Matcha', 'BioTorrents.de Stylesheet', '1', NULL, '#000000'), (2, 'Oppai', 'Oppaitime Stylesheet', '0', NULL, '#FBC2E5'), (3, 'Beluga', 'Beluga Stylesheet', '0', 'Pink|Haze', '#23252A'), (4, 'Genaviv', 'Genaviv Stylesheet', '0', 'Fixed Header', '#0A0A0A');
+-- Line 1791
