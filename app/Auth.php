c5400f72512c5c19301f8774d35ce63b84546ec3
pjc09h <48838413+pjc09h@users.noreply.github.com>
2022-05-29 11:18:31 -0400

    Esc class

--- app/Auth.php
+++ app/Auth.php
@@ -1,19 +1,27 @@
 <?php
 declare(strict_types = 1);
 
+
 /**
  * Auth
  *
  * Secure auth built on delight-im/auth.
  * Replaces various homebrew components.
  *
+ * This class dies out instead of errors out.
+ * After PHP-Auth's examples: if bad, GTFO.
+ *
  * @see https://github.com/delight-im/PHP-Auth
  */
 
-class Auth
+class Auth # extends Delight\Auth\Auth
 {
-    # hash algo for passwords
-    private static $algorithm = "sha3-512";
+    # library instance
+    private $auth = null;
+
+    # seconds * minutes * hours * days
+    private $shortRemember = 60 * 60 * 24 * 1;
+    private $longRemember = 60 * 60 * 24 * 7;
 
 
     /**
@@ -27,9 +35,14 @@ class Auth
 
         try {
             $this->auth = new Delight\Auth\Auth($app->dbOld);
-            return $this->auth;
         } catch (Exception $e) {
-            return $e->getMessage();
+            die($e->getMessage());
+        }
+
+        if ($this->auth) {
+            return true;
+        } else {
+            return false;
         }
     }
 
@@ -49,51 +62,134 @@ class Auth
         try {
             # disallow registration if the database is encrypted
             if (!apcu_exists("DBKEY")) {
-                throw new Exception("Registration temporarily disabled due to degraded database access");
+                die("Registration temporarily disabled due to degraded database access");
             }
 
             # disallow registration without invite if site is closed
             if (!$app->env->OPEN_REGISTRATION && empty($get["invite"])) {
-                throw new Exception("Open registration is disabled, no invite code provided");
+                die("Open registration is disabled, no invite code provided");
             }
         
             # you may want to exclude non-printing control characters and certain printable special characters
             if (preg_match("/[\x00-\x1f\x7f\/:\\\\]/", $post["username"]) === 1) {
-                throw new Exception("Registering usernames with control characters isn't allowed");
+                die("Registering usernames with control characters isn't allowed");
             }
 
             # don't allow a username of "0" or "1" due to PHP's type juggling
             if (trim($post["username"]) === "0" || trim($post["username"]) === "1") {
-                throw new Exception("You can't have a username of 0 or 1");
+                die("You can't have a username of 0 or 1");
             }
             
+            # if you want to enforce unique usernames, simply call registerWithUniqueUsername instead of register, and be prepared to catch the DuplicateUsernameException
             $userId = $auth->registerWithUniqueUsername($post["email"], $post["password"], $post["username"], function ($selector, $token) {
                 $app = App::go();
 
+                $get = Http::query("get");
+                $post = Http::query("post");
+
                 # build the verification uri
-                $selector = urlencode($selector);
-                $token = urlencode($token);
-                $verifyUri = "https://{$app->env->SITE_DOMAIN}/verify/{$selector}/{$token}";
+                $verifyUri = urlencode("https://{$app->env->SITE_DOMAIN}/verify/{$selector}/{$token}");
 
                 # email it to the prospective user
-                echo "Send " . $selector . " and " . $token . " to the user (e.g. via email)";
-                echo "  For emails, consider using the mail(...) function, Symfony Mailer, Swiftmailer, PHPMailer, etc.";
-                echo "  For SMS, consider using a third-party service and a compatible SDK";
+                $to = $post["email"];
+                $subject = "Your new {$app->env->SITE_NAME} registration";
+                $body = $app->twig->render("email/verifyRegistration.twig", ["env" => $app->env, "verifyUri" => $verifyUri]);
+
+                App::email($to, $subject, $body);
+                Announce::slack("{$to}\n{$subject}\n{$body}", ["debug"]);
             });
-        
-            echo "We have signed up a new user with the ID " . $userId;
         } catch (Delight\Auth\InvalidEmailException $e) {
-            return $e->getMessage();
+            die($e->getMessage());
         } catch (Delight\Auth\InvalidPasswordException $e) {
-            return $e->getMessage();
+            die($e->getMessage());
         } catch (Delight\Auth\UserAlreadyExistsException $e) {
-            return $e->getMessage();
+            die($e->getMessage());
         } catch (Delight\Auth\TooManyRequestsException $e) {
-            return $e->getMessage();
+            die($e->getMessage());
         } catch (Delight\Auth\DuplicateUsernameException $e) {
-            return $e->getMessage();
+            die($e->getMessage());
         } catch (Exception $e) {
-            return $e->getMessage();
+            die($e->getMessage());
+        }
+
+        # okay test
+        !d($userId);
+        return $userId;
+    } # register
+
+
+    /**
+     * login
+     *
+     * @see https://github.com/delight-im/PHP-Auth#login-sign-in
+     */
+    public function login()
+    {
+        $app = App::go();
+
+        $get = Http::query("get");
+        $post = Http::query("post");
+
+        try {
+            # simply call the method loginWithUsername instead of method login
+            # make sure to catch both UnknownUsernameException and AmbiguousUsernameException
+            $auth->loginWithUsername($post['username'], $post['passphrase'], $this->remember());
+        } catch (Delight\Auth\InvalidEmailException $e) {
+            die($e->getMessage());
+        } catch (Delight\Auth\InvalidPasswordException $e) {
+            die($e->getMessage());
+        } catch (Delight\Auth\EmailNotVerifiedException $e) {
+            die($e->getMessage());
+        } catch (Delight\Auth\TooManyRequestsException $e) {
+            die($e->getMessage());
+        }
+    } # login
+
+
+    /**
+     * confirmEmail
+     */
+    public function confirmEmail()
+    {
+        $app = App::go();
+
+        $get = Http::query("get");
+        $post = Http::query("post");
+
+        try {
+            # https://github.com/delight-im/PHP-Auth#keeping-the-user-logged-in
+
+            # if you want the user to be automatically signed in after successful confirmation,
+            # just call confirmEmailAndSignIn instead of confirmEmail
+            $auth->confirmEmailAndSignIn($get['selector'], $get['token'], $this->remember());
+        } catch (Delight\Auth\InvalidSelectorTokenPairException $e) {
+            die($e->getMessage());
+        } catch (Delight\Auth\TokenExpiredException $e) {
+            die($e->getMessage());
+        } catch (Delight\Auth\UserAlreadyExistsException $e) {
+            die($e->getMessage());
+        } catch (Delight\Auth\TooManyRequestsException $e) {
+            die($e->getMessage());
+        }
+    } # confirmEmail
+
+
+    /**
+     * remember
+     *
+     * @see https://github.com/delight-im/PHP-Auth#keeping-the-user-logged-in
+     */
+    public function remember()
+    {
+        $app = App::go();
+
+        $get = Http::query("get");
+        $post = Http::query("post");
+
+        if (Esc::int($post['remember']) === 1) {
+            return $this->longRemember;
+        } else {
+            return $this->shortRemember;
         }
     }
 
---
+++ app/Esc.php
@@ -0,0 +1,121 @@
+<?php
+declare(strict_types = 1);
+
+
+/**
+ * Esc
+ *
+ * Simple static class for UGC validation.
+ * Mostly a filter_var wrapper class.
+ * Also handles type coercion on return.
+ *
+ * @see https://www.php.net/manual/en/filter.filters.sanitize.php
+ */
+
+class Esc
+{
+    /** FILTER_SANITIZE */
+
+
+    /**
+     * email
+     */
+    public static function email(mixed $unsafe): string
+    {
+        $safe = filter_var($unsafe, FILTER_SANITIZE_EMAIL);
+        return strval($safe);
+    }
+
+
+    /**
+     * float
+     */
+    public static function float(mixed $unsafe): float
+    {
+        $safe = filter_var($unsafe, FILTER_SANITIZE_NUMBER_FLOAT);
+        return floatval($safe);
+    }
+
+
+    /**
+     * int
+     */
+    public static function int(mixed $unsafe): int
+    {
+        $safe = filter_var($unsafe, FILTER_SANITIZE_NUMBER_INT);
+        return intval($safe);
+    }
+
+
+    /**
+     * string
+     */
+    public static function string(mixed $unsafe): string
+    {
+        $safe = filter_var($unsafe, FILTER_SANITIZE_STRING);
+        return strval($safe);
+    }
+
+
+    /**
+     * url
+     */
+    public static function url(mixed $unsafe): string
+    {
+        $safe = filter_var($unsafe, FILTER_SANITIZE_URL);
+        return strval($safe);
+    }
+
+
+    /** FILTER_VALIDATE */
+
+
+    /**
+     * bool
+     */
+    public static function bool(mixed $unsafe): bool
+    {
+        $safe = filter_var($unsafe, FILTER_VALIDATE_BOOLEAN);
+        return boolval($safe);
+    }
+
+
+    /**
+     * domain
+     */
+    public static function domain(mixed $unsafe): string
+    {
+        $safe = filter_var($unsafe, FILTER_VALIDATE_DOMAIN);
+        return strval($safe);
+    }
+
+
+    /**
+     * ip
+     */
+    public static function ip(mixed $unsafe): string
+    {
+        $safe = filter_var($unsafe, FILTER_VALIDATE_IP);
+        return strval($safe);
+    }
+
+
+    /**
+     * mac
+     */
+    public static function mac(mixed $unsafe): string
+    {
+        $safe = filter_var($unsafe, FILTER_VALIDATE_MAC);
+        return strval($safe);
+    }
+
+
+    /**
+     * regex
+     */
+    public static function regex(mixed $unsafe): string
+    {
+        $safe = filter_var($unsafe, FILTER_VALIDATE_REGEXP);
+        return strval($safe);
+    }
+} # class
