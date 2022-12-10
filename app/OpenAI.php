5c99ebfe9dc4b0bd70147aa0b7ae4c51aeac0c80
pjc09h <48838413+pjc09h@users.noreply.github.com>
2022-12-10 12:20:12 -0500

    ugh

--- app/OpenAI.php
+++ app/OpenAI.php
@@ -87,20 +87,35 @@ class OpenAI
             throw new \Exception("groupId {$groupId} not found");
         }
 
-        # https://github.com/phpbb/phpbb/blob/master/phpBB/includes/functions_content.php
-        # strip_bbcode
         /*
+        # strip_bbcode
+        # https://github.com/phpbb/phpbb/blob/master/phpBB/includes/functions_content.php
         $text = $description;
+
         $uid = '[0-9a-z]{5,}';
         $text = preg_replace("#\[\/?[a-z0-9\*\+\-]+(?:=(?:&quot;.*&quot;|[^\]]*))?(?::[a-z])?(\:$uid)\]#", ' ', $text);
-		$match = get_preg_expression('bbcode_htm');
+
+        # get_preg_expression
+        # https://github.com/phpbb/phpbb/blob/master/phpBB/includes/functions.php
+        $match = array(
+				'#<!\-\- e \-\-><a href="mailto:(.*?)">.*?</a><!\-\- e \-\->#',
+				'#<!\-\- l \-\-><a (?:class="[\w-]+" )?href="(.*?)(?:(&amp;|\?)sid=[0-9a-f]{32})?">.*?</a><!\-\- l \-\->#',
+				'#<!\-\- ([mw]) \-\-><a (?:class="[\w-]+" )?href="http://(.*?)">\2</a><!\-\- \1 \-\->#',
+				'#<!\-\- ([mw]) \-\-><a (?:class="[\w-]+" )?href="(.*?)">.*?</a><!\-\- \1 \-\->#',
+				'#<!\-\- s(.*?) \-\-><img src="\{SMILIES_PATH\}\/.*? \/><!\-\- s\1 \-\->#',
+				'#<!\-\- .*? \-\->#s',
+				'#<.*?>#s',
+		);
+
 		$replace = array('\1', '\1', '\2', '\1', '', '');
 		$text = preg_replace($match, $replace, $text);
+
+        !d($text);exit;
         */
 
         $response = $this->client->completions()->create([
             "model" => $this->model,
-            "prompt" => "Summarize in 100 words: {$desciption}",
+            "prompt" => "Summarize in 100 words: {$description}",
             "max_tokens" => $this->maxTokens,
             "temperature" => 0,
         ]);
