1c7729f7b0c00817cf7c9e2d618ef7f6bd188320
pjc09h <48838413+pjc09h@users.noreply.github.com>
2022-12-10 12:14:48 -0500

    need to remove legacy bbcode from prompts

--- app/OpenAI.php
+++ app/OpenAI.php
@@ -29,24 +29,28 @@ namespace Gazelle;
 
 class OpenAI
 {
-    private $baseUri = "https://api.openai.com/v1/";
+    # client and params
     private $client = null;
-    private $maxTokens = 500;
+    private $maxTokens = 1000;
     private $model = "text-davinci-003";
+
+    # cache
+    private $cachePrefix = "openai_";
+    private $cacheDuration = 3600; # one hour
 
 
     /**
      * __construct
      */
     function __construct(array $options = []) {
-        $app = App::go();
+        $app = \App::go();
 
-        if (!$app->enableOpenAi) {
-            throw new Exception("OpenAI support is disabled in the app config");
+        if (!$app->env->enableOpenAi) {
+            throw new \Exception("OpenAI support is disabled in the app config");
         }
 
         $openAiApi = $app->env->getPriv("openAiApi");
-        $this->client = OpenAI::client( $openAiApi["secretKey"] );
+        $this->client = \OpenAI::client( $openAiApi["secretKey"] );
 
         return $this;
     }
@@ -59,7 +63,45 @@ class OpenAI
         $response = $this->client->completions()->create([
             "model" => $this->model,
             "prompt" => "say hello",
-            "max_tokens" => 6,
+            "max_tokens" => $this->maxTokens,
+            "temperature" => 0,
+        ]);
+
+        return $response;
+    }
+
+
+    /**
+     * summarize
+     * 
+     * Generates a summary of a torrent group description.
+     * Stores the summary in the database for later use.
+     * 
+     * @see https://beta.openai.com/docs/api-reference/completions
+     */
+    function summarize(int $groupId) {
+        $app = \App::go();
+
+        $description = $app->dbNew->single("select description from torrents_group where id = ?", [$groupId]);
+        if (!$description) {
+            throw new \Exception("groupId {$groupId} not found");
+        }
+
+        # https://github.com/phpbb/phpbb/blob/master/phpBB/includes/functions_content.php
+        # strip_bbcode
+        /*
+        $text = $description;
+        $uid = '[0-9a-z]{5,}';
+        $text = preg_replace("#\[\/?[a-z0-9\*\+\-]+(?:=(?:&quot;.*&quot;|[^\]]*))?(?::[a-z])?(\:$uid)\]#", ' ', $text);
+		$match = get_preg_expression('bbcode_htm');
+		$replace = array('\1', '\1', '\2', '\1', '', '');
+		$text = preg_replace($match, $replace, $text);
+        */
+
+        $response = $this->client->completions()->create([
+            "model" => $this->model,
+            "prompt" => "Summarize in 100 words: {$desciption}",
+            "max_tokens" => $this->maxTokens,
             "temperature" => 0,
         ]);
 
