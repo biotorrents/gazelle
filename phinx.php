4835beff42814ae510e5c7e8a57015a10b6dc9d2
pjc09h <48838413+pjc09h@users.noreply.github.com>
2022-12-18 09:14:08 -0500

    phinx config

--- phinx.php
+++ phinx.php
@@ -1,41 +1,64 @@
 <?php
 
+declare(strict_types=1);
+
+
+/**
+ * Phinx config
+ */
+
+# composer autoload
+require_once __DIR__."/../vendor/autoload.php";
+
+# load the app
+$app = App::go();
+
+# config array
 return
 [
-    'paths' => [
-        'migrations' => '%%PHINX_CONFIG_DIR%%/db/migrations',
-        'seeds' => '%%PHINX_CONFIG_DIR%%/db/seeds'
+    "paths" => [
+        "migrations" => "{$app->env->serverRoot}/database/migrations",
+        "seeds" => "{$app->env->serverRoot}/database/seeds",
     ],
-    'environments' => [
-        'default_migration_table' => 'phinxlog',
-        'default_environment' => 'development',
-        'production' => [
-            'adapter' => 'mysql',
-            'host' => 'localhost',
-            'name' => 'production_db',
-            'user' => 'root',
-            'pass' => '',
-            'port' => '3306',
-            'charset' => 'utf8',
+
+    "environments" => [
+        "default_migration_table" => "phinxlog",
+        "default_environment" => "development",
+
+        # production and development are the same in the config
+        # they transparently switch databases on $app->env->dev
+        "production" => [
+            "adapter" => "mysql",
+            "host" => $app->env->getPriv("sqlHost"),
+            "name" => $app->env->getPriv("sqlDatabase"),
+            "user" => $app->env->getPriv("sqlUsername"),
+            "pass" => $app->env->getPriv("sqlPassphrase"),
+            "port" => $app->env->getPriv("sqlPort"),
+            "charset" => "utf8",
         ],
-        'development' => [
-            'adapter' => 'mysql',
-            'host' => 'localhost',
-            'name' => 'development_db',
-            'user' => 'root',
-            'pass' => '',
-            'port' => '3306',
-            'charset' => 'utf8',
+
+        "development" => [
+            "adapter" => "mysql",
+            "host" => $app->env->getPriv("sqlHost"),
+            "name" => $app->env->getPriv("sqlDatabase"),
+            "user" => $app->env->getPriv("sqlUsername"),
+            "pass" => $app->env->getPriv("sqlPassphrase"),
+            "port" => $app->env->getPriv("sqlPort"),
+            "charset" => "utf8",
         ],
-        'testing' => [
-            'adapter' => 'mysql',
-            'host' => 'localhost',
-            'name' => 'testing_db',
-            'user' => 'root',
-            'pass' => '',
-            'port' => '3306',
-            'charset' => 'utf8',
-        ]
+
+        /*
+        "testing" => [
+            "adapter" => "mysql",
+            "host" => "localhost",
+            "name" => "testing_db",
+            "user" => "root",
+            "pass" => "",
+            "port" => "3306",
+            "charset" => "utf8",
+        ],
+        */
     ],
-    'version_order' => 'creation'
+
+    "version_order" => "creation"
 ];
