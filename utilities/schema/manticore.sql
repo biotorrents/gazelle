a7f4737f9e93325739c61d31de265609c37ca97e
pjc09h <48838413+pjc09h@users.noreply.github.com>
2023-01-03 19:40:01 -0500

    draft manticore schemata

---
+++ utilities/schema/manticore.sql
@@ -0,0 +1,199 @@
+-- --
+-- manticore search engine schema
+--
+
+
+-- manticore_counter
+create table `manticore_counter` (
+  `type` varchar(16) not null,
+  `lastTouch` int,
+
+  primary key (`type`)
+) engine=InnoDB charset=utf8mb4;
+
+
+-- manticore_torrents_group
+create table `manticore_torrents_group` (
+  `id` int not null,
+  `categoryId` tinyint,
+  `title` varchar(255),
+  `subject` varchar(255),
+  `object` varchar(255),
+  `year` smallint,
+  `workgroup` varchar(128),
+  `location` varchar(128),
+  `identifier` varchar(64),
+  `tagList` text,
+  `groupDescription` text,
+  `picture` varchar(255),
+
+  primary key (`id`)
+) engine=InnoDB charset=utf8mb4;
+
+
+-- manticore_torrents
+create table `manticore_torrents` (
+  `id` int not null,
+  `groupId` int not null,
+  `userId` int,
+  `platform` varchar(32),
+  `format` varchar(32),
+  `license` varchar(32),
+  `scope` varchar(32),
+  `version` varchar(16),
+  `aligned` tinyint,
+  `infoHash` blob,
+  `fileList` text,
+  `size` bigint,
+  `leechers` int not null,
+  `seeders` int not null,
+  `leechStatus` tinyint not null,
+  `timeAdded` int,
+  `torrentDescription` text,
+  `snatches` int,
+  `archive` varchar(16),
+
+  primary key (`id`),
+  key `groupId` (`groupId`)
+) engine=InnoDB charset=utf8mb4;
+
+
+-- manticore_torrents_delta
+create table `manticore_torrents_delta` (
+  -- torrents_group
+  `id` int not null,
+  `categoryId` tinyint,
+  `title` varchar(255),
+  `subject` varchar(255),
+  `object` varchar(255),
+  `year` smallint,
+  `workgroup` varchar(128),
+  `location` varchar(128),
+  `identifier` varchar(64),
+  `tagList` text,
+  `groupDescription` text,
+  `picture` varchar(255),
+
+  -- torrents
+  `groupId` int not null,
+  `userId` int,
+  `platform` varchar(32),
+  `format` varchar(32),
+  `license` varchar(32),
+  `scope` varchar(32),
+  `version` varchar(16),
+  `aligned` tinyint,
+  `infoHash` blob,
+  `fileList` text,
+  `size` bigint not null,
+  `leechers` int,
+  `seeders` int,
+  `leechStatus` tinyint,
+  `timeAdded` int,
+  `torrentDescription` text,
+  `snatches` int,
+  `archive` varchar(16),
+
+  -- manticore_creators
+  `creatorList` text,
+
+  primary key (`id`),
+  key `groupId` (`groupId`),
+  key `size` (`size`)
+) engine=InnoDB charset=utf8mb4;
+
+
+-- manticore_creators
+create table `manticore_creators` (
+  `groupId` int not null,
+  `creatorList` text,
+
+  key `groupId` (`groupId`)
+) engine=InnoDB charset=utf8mb4;
+
+
+-- manticore_creators_delta
+-- todo?
+
+
+-- manticore_requests
+create table `manticore_requests` (
+  -- requests
+  `id` int not null,
+  `userId` int not null,
+  `timeAdded` int not null,
+  `lastVote` int not null,
+  `categoryId` smallint,
+  `title` varchar(255),
+  `subject` varchar(255),
+  `object` varchar(255),
+  `picture` varchar(255),
+  `description` text,
+  `identifier` varchar(64),
+  `fillerId` int not null,
+  `torrentId` int,
+  `timeFilled` int,
+  `visible` binary(1),
+
+  -- requests_votes
+  `bounty` bigint,
+  `voteCount` bigint,
+
+  -- manticore_creators
+  `creatorList` text,
+
+  primary key (`id`),
+  key `userId` (`userId`),
+  key `timeAdded` (`timeAdded`),
+  key `lastVote` (`lastVote`),
+  key `fillerId` (`fillerId`),
+  key `timeFilled` (`timeFilled`)
+) engine=InnoDB charset=utf8mb4;
+
+
+-- manticore_requests_delta
+create table `manticore_requests_delta` (
+  -- requests
+  `id` int not null,
+  `userId` int not null,
+  `timeAdded` int not null,
+  `lastVote` int not null,
+  `categoryId` smallint,
+  `title` varchar(255),
+  `subject` varchar(255),
+  `object` varchar(255),
+  `picture` varchar(255),
+  `description` text,
+  `identifier` varchar(64),
+  `fillerId` int not null,
+  `torrentId` int,
+  `timeFilled` int,
+  `visible` binary(1),
+
+  -- requests_votes
+  `bounty` bigint,
+  `voteCount` bigint,
+
+  -- manticore_creators
+  `creatorList` text,
+
+  primary key (`id`),
+  key `userId` (`userId`),
+  key `timeAdded` (`timeAdded`),
+  key `lastVote` (`lastVote`),
+  key `fillerId` (`fillerId`),
+  key `timeFilled` (`timeFilled`)
+) engine=InnoDB charset=utf8mb4;
+
+
+-- --
+-- to drop the tables
+--
+
+drop table manticore_counter;
+drop table manticore_torrents_group;
+drop table manticore_torrents;
+drop table manticore_torrents_delta;
+drop table manticore_creators;
+drop table manticore_requests;
+drop table manticore_requests_delta;
