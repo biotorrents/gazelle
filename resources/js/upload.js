7859cd018b6f3d20d2da8395b9a1d90beaf6fc8f
pjc09h <48838413+pjc09h@users.noreply.github.com>
2023-02-03 13:35:57 -0500

    cleanup javascript and add spinner

--- resources/js/upload.js
+++ resources/js/upload.js
@@ -5,11 +5,14 @@
      * change fields on category select
      */
 
-    // trigger categoryId change on load
     $(() => {
+        // trigger categoryId change on load
         $("#categoryId").trigger("change", (event) => {
             $(event.target).val(1);
         });
+
+        // hide ajax spinner
+        $("#autofillLoader").hide();
     });
 
     // update select elements on category change
@@ -82,6 +85,9 @@
      */
 
     $("#doiNumberAutofill").on("click", () => {
+        // show ajax spinner
+        $("#autofillLoader").show();
+
         // the data to send
         var request = {
             frontendHash: frontendHash,
@@ -95,6 +101,9 @@
 
         // ajax request
         $.post("/api/internal/doiNumberAutofill", request, (response) => {
+            // hide ajax spinner
+            $("#autofillLoader").hide();
+
             if (response.status === "success") {
                 $("#title").val(response.data.title);
                 $("#groupDescription").html(response.data.groupDescription);
@@ -113,59 +122,10 @@
 
 
     /**
-     * AddArtistField
-     */
-    var ArtistCount = 1;
-    function AddArtistField() {
-        window.getSelection().removeAllRanges();
-        ArtistCount = $('input[name="artists[]"]').length;
-
-        if (ArtistCount >= 200) {
-            return;
-        }
-
-        var ArtistField = document.createElement("input");
-        ArtistField.type = "text";
-        ArtistField.id = "artist_" + ArtistCount;
-        ArtistField.name = "artists[]";
-        ArtistField.size = 45;
-
-        var x = $("#artistfields").raw();
-        x.appendChild(document.createElement("br"));
-        x.appendChild(ArtistField);
-        x.appendChild(document.createTextNode("\n"));
-
-        if ($("#artist_0").data("gazelle-autocomplete")) {
-            $(ArtistField).on("focus", function () {
-                $(ArtistField).autocomplete({
-                    serviceUrl: ARTIST_AUTOCOMPLETE_URL,
-                });
-            });
-        }
-        ArtistCount++;
-    }
-
-    /**
-     * RemoveArtistField
-     */
-    function RemoveArtistField() {
-        window.getSelection().removeAllRanges();
-        ArtistCount = $('input[name="artists[]"]').length;
-
-        if (ArtistCount == 1) {
-            return;
-        }
-
-        var x = $("#artistfields").raw();
-        for (i = 0; i < 3; i++) {
-            x.removeChild(x.lastChild);
-        }
-        ArtistCount--;
-    }
-
-    /**
      * AddScreenshotField
      */
+
+    /*
     function AddScreenshotField() {
         var sss = $('[name="screenshots[]"]');
         if (sss.length >= 10) return;
@@ -190,147 +150,26 @@
         y.appendChild(a);
         x.appendChild(y);
     }
+    */
+
 
     /**
      * RemoveScreenshotField
      */
+
+    /*
     function RemoveScreenshotField(el) {
         var sss = $('[name="screenshots[]"]');
         el.parentElement.remove();
     }
+    */
 
-    /**
-     * AnimeAutofill
-     */
-    function AnimeAutofill() {
-        var map = {
-            artist: "artist_0",
-            title: "title",
-            title_rj: "title_rj",
-            title_jp: "title_jp",
-            year: "year",
-            description: "album_desc",
-        };
-
-        var aid = $("#anidb").raw().value;
-        $.getJSON("/api.php?action=autofill&cat=anime&aid=" + aid, function (data) {
-            if (data.status != "success") return;
-            for (i in data.response) {
-                if (map[i] && !$("#" + map[i]).raw().value) {
-                    $("#" + map[i]).raw().value = data.response[i];
-                }
-            }
-        });
-    }
-
-    /**
-     * JavAutofill
-     */
-    function JavAutofill() {
-        var map = {
-            cn: "javdb",
-            artists: "artists",
-            title: "title",
-            title_jp: "title_jp",
-            year: "year",
-            studio: "studio",
-            image: "image",
-            tags: "tags",
-            description: "album_desc",
-        };
-
-        var cn = $("#javdb_tr #catalogue").raw().value.toUpperCase();
-        $.getJSON("/api.php?action=autofill&cat=jav&cn=" + cn, function (data) {
-            if (data.status != "success") {
-                $("#catalogue").raw().value = "Failed";
-                return;
-            } else {
-                $("#catalogue").raw().value = data.response.cn;
-            }
-
-            for (i in data.response) {
-                if (Array.isArray(data.response[i])) {
-                    for (j in data.response[i]) {
-                        if (i == "artists") {
-                            if (!$("#" + map[i] + "_" + j).raw()) {
-                                AddArtistField();
-                            }
-                            $("#" + map[i] + "_" + j).raw().value = data.response[i][j];
-                        }
-                        if (map[i] == "tags" && !$("#" + map[i]).raw().value) {
-                            $("#" + map[i]).raw().value = data.response[i].join(", ");
-                        }
-                    }
-                }
-
-                if (map[i] && $("#" + map[i]).raw() && !$("#" + map[i]).raw().value) {
-                    $("#" + map[i]).raw().value = data.response[i];
-                }
-            }
-
-            if (data.response.screens.length) {
-                $("#album_desc").raw().value =
-                    "[spoiler=Automatically located thumbs][img]" +
-                    data.response.screens.join("[/img][img]") +
-                    "[/img][/spoiler]\n\n" +
-                    $("#album_desc").raw().value;
-            }
-        });
-    }
-
-    /**
-     * MangaAutofill
-     */
-    function MangaAutofill() {
-        var map = {
-            artists: "artists",
-            title: "title",
-            title_jp: "title_jp",
-            year: "year",
-            tags: "tags",
-            lang: "lang",
-            cover: "image",
-            circle: "series",
-            pages: "pages",
-            description: "release_desc",
-        };
-
-        var nh = $("#ehentai_tr #catalogue").raw().value;
-        $.getJSON("/api.php?action=autofill&cat=manga&url=" + nh, function (data) {
-            if (data.status != "success") {
-                $("#catalogue").raw().value = "Failed";
-                return;
-            }
-
-            for (i in data.response) {
-                if (Array.isArray(data.response[i])) {
-                    for (j in data.response[i]) {
-                        if (i == "artists") {
-                            if (!$("#" + map[i] + "_" + j).raw()) {
-                                AddArtistField();
-                            }
-                            $("#" + map[i] + "_" + j).raw().value = data.response[i][j];
-                        }
-                        if (map[i] == "tags" && !$("#" + map[i]).raw().value) {
-                            $("#" + map[i]).raw().value = data.response[i].join(", ");
-                        }
-                    }
-                }
-
-                if (
-                    map[i] &&
-                    $("#" + map[i]).raw() &&
-                    (!$("#" + map[i]).raw().value || $("#" + map[i]).raw().value == "---")
-                ) {
-                    $("#" + map[i]).raw().value = data.response[i];
-                }
-            }
-        });
-    }
 
     /**
      * SetResolution
      */
+
+    /*
     function SetResolution() {
         if ($("#ressel").raw().value != "Other") {
             $("#resolution").raw().value = $("#ressel").raw().value;
@@ -341,6 +180,5 @@
             $("#resolution").raw().readOnly = false;
         }
     }
-
-
+    */
 })();
