96289a376a4e1a906d550d9f32da6002545fa465
pjc09h <48838413+pjc09h@users.noreply.github.com>
2023-04-07 13:31:15 -0400

    group

---
+++ app/Models/Group.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+
+/**
+ * Gazelle\Models\Group
+ */
+
+namespace Gazelle\Models;
+
+class Group extends Base
+{
+    # https://laravel.com/docs/master/eloquent#uuid-and-ulid-keys
+    #use \Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+    # the table associated with the model
+    protected $table = "torrents_group";
+
+    # the primary key associated with the table
+    protected $primaryKey = "id";
+
+    # indicates if the model should be timestamped
+    public $timestamps = false;
+
+    # the attributes that aren't mass assignable
+    protected $guarded = ["id"];
+} # class
