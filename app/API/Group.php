9d922d079c16fa84a70d34c3806d1debd8ba5a09
pjc09h <48838413+pjc09h@users.noreply.github.com>
2023-05-21 02:20:58 +0000

    mock up some more crud stuff

---
+++ app/API/Group.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+
+/**
+ * Gazelle\API\Group
+ */
+
+namespace Gazelle\API;
+
+class Group extends Base
+{
+    /**
+     * create
+     */
+    public static function create(): void
+    {
+        self::validatePermissions($_SESSION["token"]["id"], ["create"]);
+
+        self::failure(400, "not implemented");
+    }
+
+
+    /**
+     * read
+     */
+    public static function read(int|string $identifier): void
+    {
+        self::validatePermissions($_SESSION["token"]["id"], ["read"]);
+
+        self::failure(400, "not implemented");
+    }
+
+
+    /**
+     * update
+     */
+    public static function update(): void
+    {
+        self::validatePermissions($_SESSION["token"]["id"], ["update"]);
+
+        self::failure(400, "not implemented");
+    }
+
+
+    /**
+     * delete
+     */
+    public static function delete(): void
+    {
+        self::validatePermissions($_SESSION["token"]["id"], ["delete"]);
+
+        self::failure(400, "not implemented");
+    }
+} # class
